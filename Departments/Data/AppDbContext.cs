80ee3a520f60ddf4a2ca26d5a1722c7a3cf8f020
Igor Mogilnitskiy <igmo71@outlook.com>
2025-02-10 10:14:20 +0300

    ПользователиСхемаПредприятия

--- Departments/Data/AppDbContext.cs
+++ Departments/Data/AppDbContext.cs
@@ -5,18 +5,20 @@ namespace Departments.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext (DbContextOptions<AppDbContext> options)
+        public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
         }
 
-        public DbSet<Departments.Domain.CatalogСхемаПредприятия> CatalogСхемаПредприятия { get; set; } = default!;
-        public DbSet<Departments.Domain.CatalogПользователи> CatalogПользователи { get; set; } = default!;
+        public DbSet<CatalogСхемаПредприятия> CatalogСхемаПредприятия { get; set; } = default!;
+        public DbSet<CatalogПользователи> CatalogПользователи { get; set; } = default!;
+        public DbSet<ПользователиСхемаПредприятия> ПользователиСхемаПредприятия { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CatalogСхемаПредприятия>().ToTable("Catalog_СхемаПредприятия").HasKey(e => e.Ref_Key);
             modelBuilder.Entity<CatalogПользователи>().ToTable("Catalog_Пользователи").HasKey(e => e.Ref_Key);
+            modelBuilder.Entity<ПользователиСхемаПредприятия>().ToTable("Пользователи_СхемаПредприятия").HasKey(e => e.Ref_Key);
         }
     }
 }
---
+++ Departments/Domain/ПользователиСхемаПредприятия.cs
@@ -0,0 +1,11 @@
+﻿using Departments.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Departments.Domain
+{
+    public class ПользователиСхемаПредприятия
+    {
+        [MaxLength(AppSettings.GUID)] public required string Ref_Key { get; set; }
+        [MaxLength(AppSettings.GUID)] public required string СхемаПредприятия_Key { get; set; }
+    }
+}
