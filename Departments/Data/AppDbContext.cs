b2869c1bf8c186e2538cbfc7027ec8e8eaad75e3
Igor Mogilnitskiy <igmo71@outlook.com>
2025-02-10 17:43:00 +0300

    Пользователь - СхемаПредприятия

--- Departments/Data/AppDbContext.cs
+++ Departments/Data/AppDbContext.cs
@@ -12,13 +12,26 @@ namespace Departments.Data
 
         public DbSet<CatalogСхемаПредприятия> CatalogСхемаПредприятия { get; set; } = default!;
         public DbSet<CatalogПользователи> CatalogПользователи { get; set; } = default!;
-        public DbSet<ПользователиСхемаПредприятия> ПользователиСхемаПредприятия { get; set; }
+        public DbSet<ПользовательСхемаПредприятия> ПользователиСхемаПредприятия { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CatalogСхемаПредприятия>().ToTable("Catalog_СхемаПредприятия").HasKey(e => e.Ref_Key);
+
             modelBuilder.Entity<CatalogПользователи>().ToTable("Catalog_Пользователи").HasKey(e => e.Ref_Key);
-            modelBuilder.Entity<ПользователиСхемаПредприятия>().ToTable("Пользователи_СхемаПредприятия").HasKey(e => e.Ref_Key);
+
+            modelBuilder.Entity<ПользовательСхемаПредприятия>().ToTable("Пользователь_СхемаПредприятия").HasKey(e => e.Пользователь_Key);
+            modelBuilder.Entity<ПользовательСхемаПредприятия>()
+                .HasOne(e => e.Пользователь)
+                .WithOne()
+                .HasForeignKey<ПользовательСхемаПредприятия>(e => e.Пользователь_Key)
+                .HasPrincipalKey<CatalogПользователи>(e => e.Ref_Key);
+            modelBuilder.Entity<ПользовательСхемаПредприятия>()
+                .HasOne(e => e.СхемаПредприятия)
+                .WithOne()
+                .HasForeignKey<ПользовательСхемаПредприятия>(e => e.СхемаПредприятия_Key)
+                .HasPrincipalKey<CatalogСхемаПредприятия>(e => e.Ref_Key);
+
         }
     }
 }
---
+++ Departments/Domain/ПользовательСхемаПредприятия.cs
@@ -0,0 +1,17 @@
+﻿using Departments.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Departments.Domain
+{
+    public class ПользовательСхемаПредприятия
+    {
+        [MaxLength(AppSettings.GUID)] public string? Пользователь_Key { get; set; }
+
+        public CatalogПользователи? Пользователь { get; set; }
+
+
+        [MaxLength(AppSettings.GUID)] public string? СхемаПредприятия_Key { get; set; }
+
+        public CatalogСхемаПредприятия? СхемаПредприятия { get; set; }
+    }
+}
